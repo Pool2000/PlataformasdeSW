d6948f3512fab2e0201e095cd2ed73388b4329da
Pool2000 <54323899+Pool2000@users.noreply.github.com>
2022-08-09 23:16:46 -0500

    Delete install2.bat

--- install2.bat
+++
@@ -1,8 +0,0 @@
-echo Instalador de la base de datos Universidad
-echo Autor: Pool Andre
-echo 8 de agosto de 2022
-sqlcmd -S. -E -i BDUniversidad.sql
-sqlcmd -S. -E -i BDUniversidadPA.sql
-sqlcmd -S. -E -i BDUniversidadPA2.sql
-echo Se ejecuto correctamente la base de datos
-pause
